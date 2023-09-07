40eeb27a17e5231987ca92170d92a8f59f966cc2
Kara-Dobriyal <hatakekaran@gmail.com>
2023-09-07 22:11:00 +0530

    Highest Lowest Freq

--- 1. The Basics/3. Hashing/Find the highestlowest frequency element/HighLowFreq.java
+++ 1. The Basics/3. Hashing/Find the highestlowest frequency element/HighLowFreq.java
@@ -11,6 +11,53 @@
 // Output: 2 3
 // Explanation: The frequency of 2 is 3, i.e. the highest and the frequency of 3 is 1 i.e. the lowest.
 
+import java.util.*;
+
 public class HighLowFreq {
+
+public static void getFrequencies(int []v) {
+
+    Map<Integer,Integer>M=new HashMap<>();
+
+    for(int i=0;i<v.length;i++)
+    { 
+        M.put(v[i], M.getOrDefault(v[i],0)+1); 
+    }
+
+    int Maxfreq=0; 
+    int Minfreq=v.length; 
+    int MaxEle=0; 
+    int MinEle=Integer.MAX_VALUE; 
+    for(Map.Entry<Integer,Integer>entry:M.entrySet())
+    { 
+        int count=entry.getValue(); 
+        int element=entry.getKey(); 
     
+        if(count>Maxfreq)
+        { 
+            MaxEle=element; 
+            Maxfreq=count; 
+        }  
+        else if(count==Maxfreq)
+        { 
+            MaxEle=Math.min(MaxEle,element); 
+        }  
+        if(count<Minfreq)
+        { 
+            MinEle=element; 
+            Minfreq=count; 
+        } 
+        else if(count==Minfreq)
+        { 
+            MinEle=Math.min(MinEle, element); 
+        } 
+    }
+
+    int ans[]={MaxEle,MinEle};  
+    for(int i=0; i<ans.length; i++)
+    {
+        System.out.println(ans[i]);
+    }
+    // Time Complexity is O(V) and Space Complexity is O(N) because we are creating HashMap as an extra space.
+    } 
 }
