1a9c2ad7e12b379046a38eed70eeaa2628dab3b0
Karan Dobriyal <karandobriyal26@gmail.com>
2023-09-08 09:04:09 +0530

    First Repeat Element

--- 1. The Basics/3. Hashing/First Repeating element/FirstRepEle.java
+++ 1. The Basics/3. Hashing/First Repeating element/FirstRepEle.java
@@ -52,6 +52,35 @@
 
 //  There is no repeating element, output -1
 
+import java.util.HashMap;
+
 public class FirstRepEle {
-    
+    public static void main()
+    {
+        int[] A = {10, 5, 3, 4, 3, 5, 6};
+        int ans = -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+       for(int i=0; i<A.length; i++)
+       {
+           if(map.containsKey(A[i]))
+           {
+               map.put(A[i],map.get(A[i])+1);
+           }
+           else
+           {
+               map.put(A[i],1);
+           }
+       }
+        
+        for(int i=0; i<A.length; i++)
+        {
+            if(map.containsKey(A[i]))
+            {
+                if(map.get(A[i])>1)
+                ans = A[i];
+            }
+        }
+        System.out.println(ans);
+    }
 }
