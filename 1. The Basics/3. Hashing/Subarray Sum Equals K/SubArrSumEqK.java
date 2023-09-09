f5369daee4fb5d7dda63a32f15ef9ecb9af73ffb
Karan Dobriyal <karandobriyal26@gmail.com>
2023-09-09 16:59:00 +0530

    Subarray Sum Equals to K

--- 1. The Basics/3. Hashing/Subarray Sum Equals K/SubArrSumEqK.java
+++ 1. The Basics/3. Hashing/Subarray Sum Equals K/SubArrSumEqK.java
@@ -41,6 +41,24 @@
 // Explanation 1:
 // All the possible subarrays having sum 0.
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArrSumEqK {
-    
+    public static void main()
+    {
+        int[] A = {1, 0, 1};
+        int B = 1;
+        Map<Integer, Integer> prevSum = new HashMap<Integer, Integer>();
+        prevSum.put(0, 1);
+        int currSum = 0, ans = 0;
+        for(int i = 0 ; i < A.length ; i++){
+            currSum += A[i];
+            if(prevSum.containsKey(currSum - B)){
+                ans += prevSum.get(currSum - B);
+            }
+            prevSum.put(currSum, prevSum.getOrDefault(currSum, 0) + 1);
+        }
+        System.out.println(ans);
+    }
 }
