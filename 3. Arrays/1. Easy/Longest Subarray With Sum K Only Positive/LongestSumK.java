fe60a5a23c8a8bd5daabba67ffcefdbcf451d18c
Karan Dobriyal <karandobriyal26@gmail.com>
2023-09-27 19:44:46 +0530

    Longest Subarray with sum K

---
+++ 3. Arrays/1. Easy/Longest Subarray With Sum K Only Positive/LongestSumK.java
@@ -0,0 +1,77 @@
+// Problem Statement: Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
+
+// Examples
+// Example 1:
+// Input Format: N = 3, k = 5, array[] = {2,3,5}
+// Result: 2
+// Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+// Example 2:
+// Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+// Result: 3
+// Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+
+// Algorithm / Intuition
+// Optimal Approach (Using 2 pointers): 
+// Approach:
+// The steps are as follows:
+
+// First, we will take two pointers: left and right, initially pointing to the index 0.
+// The sum is set to a[0] i.e. the first element initially.
+// Now we will run a while loop until the right pointer crosses the last index i.e. n-1.
+// Inside the loop, we will do the following:
+// We will use another while loop and it will run until the sum is lesser or equal to k.
+// Inside this second loop, from the sum, we will subtract the element that is pointed by the left pointer and increase the left pointer by 1.
+// After this loop gets completed, we will check if the sum is equal to k. If it is, we will compare the length of the current subarray i.e. (right-left+1) with the existing one and consider the maximum one.
+// Then we will move forward the right pointer by 1. If the right pointer is pointing to a valid index(< n) after the increment, we will add the element i.e. a[right] to the sum.
+// Finally, we will return the maximum length.
+// Intuition: We are using two pointers i.e. left and right. The left pointer denotes the starting index of the subarray and the right pointer denotes the ending index. Now as we want the longest subarray, we will move the right pointer in a forward direction every time adding the element i.e. a[right] to the sum. But when the sum of the subarray crosses k, we will move the left pointer in the forward direction as well to shrink the size of the subarray as well as to decrease the sum. Thus, we will consider the length of the subarray whenever the sum becomes equal to k.
+// The below dry run will clarify the intuition.
+
+ 
+
+// This Method is only Optimal for 0<=A[i]<=N
+// Not For Negative Numbers for that use Hashing.
+import java.util.*;
+
+public class LongestSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+// Time Complexity: O(2*N), where N = size of the given array.
+// Reason: The outer while loop i.e. the right pointer can move up to index n-1(the last index). Now, the inner while loop i.e. the left pointer can move up to the right pointer at most. So, every time the inner loop does not run for n times rather it can run for n times in total. So, the time complexity will be O(2*N) instead of O(N2).
+
+// Space Complexity: O(1) as we are not using any extra space.
