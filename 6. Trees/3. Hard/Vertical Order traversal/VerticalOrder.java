cfe4c86d3ae6b6c514ae63b1240688827536257b
Karan Dobriyal <hatakekaran@gmail.com>
2023-10-18 00:26:04 +0530

    Veritcal Order

--- 6. Trees/3. Hard/Vertical Order traversal/VerticalOrder.java
+++ 6. Trees/3. Hard/Vertical Order traversal/VerticalOrder.java
@@ -64,3 +64,72 @@
 
 //  First row represent the verical line 1 and so on.
 
+ 
+
+  
+import java.util.*;
+public class VerticalOrder {
+
+    public class Node {
+        TreeNode node;
+        int value; // which defines the vertical order of node 
+
+        Node(TreeNode node, int value) {
+            this.node = node;
+            this.value = value;
+        }
+    }
+
+
+    public ArrayList<ArrayList<Integer>> verticalOrderTraversal(TreeNode A) {
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<Integer, ArrayList<Integer>>();
+        int min = 0, max = 0;
+        Queue<Node> queue = new LinkedList<Node>();
+
+        queue.add(new Node(A, 0));
+
+        while(!queue.isEmpty()) {
+
+            Node x = queue.poll();
+
+            max = Math.max(max, x.value);
+            min = Math.min(min, x.value);
+
+            if(!hm.containsKey(x.value)) {
+                ArrayList<Integer> y = new ArrayList<Integer>();
+                y.add(x.node.val);
+                hm.put(x.value, y);
+            }
+            else {
+                hm.get(x.value).add(x.node.val);
+            }
+
+            if(x.node.left != null) {
+                queue.add(new Node(x.node.left, x.value - 1));
+            }
+
+            if(x.node.right != null) {
+                queue.add(new Node(x.node.right, x.value + 1));
+            }
+        }
+        
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        for(int i = min; i <= max; i++) {
+            ans.add(hm.get(i));
+        }
+        
+        return ans;
+    }
+}
+// Time and Space Complexity : O(N)
+ // Definition for binary tree
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+       val = x;
+       left=null;
+       right=null;
+      }
+  }
