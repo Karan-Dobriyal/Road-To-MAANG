48872ccd98630321c2ae8ebadd4e55110000ec3d
Karan Dobriyal <hatakekaran@gmail.com>
2023-10-18 23:41:55 +0530

    Zig Zag Level Order

--- 6. Trees/2. Medium/ZigZag Level Order Traversal BT/ZigZagLevelOrder.java
+++ 6. Trees/2. Medium/ZigZag Level Order Traversal BT/ZigZagLevelOrder.java
@@ -55,3 +55,69 @@
 // Explanation 1:
 
 //  Return the 2D array. Each row denotes the zigzag traversal of each level.
+
+
+import java.util.*;
+public class ZigZagLevelOrder {
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode A) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(A);
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        int level = 1;
+        
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int i=0; i<size; i++)
+            {
+                TreeNode node = queue.remove();
+                temp.add(node.val);
+
+                if(node.left != null)
+                    queue.add(node.left);
+
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            if( (level & 1) == 0)
+                reverse(temp);
+            level++;
+            res.add(temp);
+        }
+        return res;
+    }
+
+    void reverse(ArrayList<Integer> temp)
+    {
+        int start = 0;
+        int end = temp.size()-1;
+
+        while(start < end)
+        {
+            temp.set(start, temp.get(start)^temp.get(end));
+            temp.set(end, temp.get(start)^temp.get(end));
+            temp.set(start, temp.get(start)^temp.get(end));
+            start++;
+            end--;
+        }
+    }
+}
+
+
+// Time and Space Complexity : O(N)
+
+ 
+ // Definition for binary tree
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+       val = x;
+       left=null;
+       right=null;
+      }
+  }
+ 
